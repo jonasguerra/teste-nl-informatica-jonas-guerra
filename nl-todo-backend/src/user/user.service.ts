698687ff061e678c88338eb780b16eea53e7b219
Jonas Guerra <guerra.jonas@hotmail.com>
2022-08-10 17:13:04 -0300

    Ajuste na validação de e-mail do usuário

--- nl-todo-backend/src/user/user.service.ts
+++ nl-todo-backend/src/user/user.service.ts
@@ -1,17 +1,22 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
-import { InjectRepository } from '@nestjs/typeorm';
-import { PageDto, PageOptionsDto } from 'src/common/dto/page.dto';
-import { Repository } from 'typeorm';
-import { User } from './entities/user.entity';
+import { BadRequestException, Injectable } from "@nestjs/common";
+import { InjectRepository } from "@nestjs/typeorm";
+import { PageDto, PageOptionsDto } from "src/common/dto/page.dto";
+import { Repository } from "typeorm";
+import { User } from "./entities/user.entity";
 
 @Injectable()
 export class UserService {
-  constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>
+  ) {}
 
   async listAll(pageOptionsDto: PageOptionsDto): Promise<PageDto<User>> {
-    const queryBuilder = this.userRepository.createQueryBuilder('user');
+    const queryBuilder = this.userRepository.createQueryBuilder("user");
 
-    queryBuilder.orderBy('user.createdAt', pageOptionsDto.order).skip(pageOptionsDto.skip).take(pageOptionsDto.take);
+    queryBuilder
+      .orderBy("user.createdAt", pageOptionsDto.order)
+      .skip(pageOptionsDto.skip)
+      .take(pageOptionsDto.take);
 
     const count = await queryBuilder.getCount();
     const { entities } = await queryBuilder.getRawAndEntities();
@@ -20,7 +25,7 @@ export class UserService {
   }
 
   findById(id: string): Promise<User> {
-    return this.userRepository.findOne(id, { relations: ['client'] });
+    return this.userRepository.findOne(id, { relations: ["client"] });
   }
 
   retriveUserByEmail(email: string): Promise<User> {
@@ -29,7 +34,8 @@ export class UserService {
 
   async create(user: User): Promise<User> {
     const userExist = await this.userRepository.findOne({ email: user.email });
-    if (userExist) throw new BadRequestException('User already registered with email');
+    if (userExist)
+      throw new BadRequestException("Usuário com este e-mail já existe");
     const createdUser = await this.userRepository.save(user);
 
     return createdUser;
